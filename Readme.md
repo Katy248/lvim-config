e2e43a290d5161e43d3575e35b8b5b2bda998e4d
Katy248 <petrovanton247@gmail.com>
2024-08-06 09:59:14 +0300

    Первый комит

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Мои `LunarVim` дотфайлы
+
+**Список дополнительных плагинов:**
+
+- `abdelrahmandwedar/awesome-nvim-colorschemes` - для цветовой схемы _gruvbox_
+- `meanderingprogrammer/markdown.nvim` - для крутого форматирования `.md`
+- `iabdelkareem/csharp.nvim` - для c#
+  
---
+++ config.lua
@@ -0,0 +1,24 @@
+vim.o.mousemoveevent = true
+
+vim.lsp.get_active_clients()
+-- visual
+lvim.colorscheme = "gruvbox"
+-- lvim.colorscheme = "rose-pine-main"
+lvim.transparent_window = true
+
+-- code
+lvim.format_on_save = true
+
+lvim.plugins = {
+  -- { "lunarvim/colorschemes" },
+  { "abdelrahmandwedar/awesome-nvim-colorschemes" },
+  -- { "rcarriga/nvim-notify" },
+  -- { "rose-pine/neovim" },
+  { "meanderingprogrammer/markdown.nvim" },
+  { "iabdelkareem/csharp.nvim" },
+  -- {
+  -- "soulis-1256/eagle.nvim",
+  -- branch = "new_features",
+  -- config = true,
+  -- },
+}
