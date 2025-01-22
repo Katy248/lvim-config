94c38e5112c80bfad68945c7ac42e417008914df
Katy248 <petrovanton247@gmail.com>
2025-01-22 12:09:29 +0300

    Cleared code

--- config.lua
+++ config.lua
@@ -8,27 +8,22 @@ lvim.colorscheme = "gruvbox"
 lvim.format_on_save = true
 vim.opt.wrap = true
 
-function test()
-end
-
 lvim.plugins = {
     -- colorschemes
     { "abdelrahmandwedar/awesome-nvim-colorschemes" },
     { "rose-pine/neovim" },
-    { "kepano/flexoki-neovim",                      name = 'flexoki' },
-    --
-    -- { "meanderingprogrammer/markdown.nvim" },
+    { 
+        "kepano/flexoki-neovim",                      
+        name = 'flexoki'
+    },
+    -- languages
     { "iabdelkareem/csharp.nvim" },
+    -- ui
     {
         "f-person/git-blame.nvim",
         event = "VeryLazy"
     },
-    -- {
-    --   "m4xshen/hardtime.nvim",
-    --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    --   opts = {},
-    -- },
-    -- { "BlackLight/nvim-http" },
+    -- other
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
@@ -45,28 +40,13 @@ lvim.plugins = {
             context = 20,
         },
     },
-    -- {
-    --     'stevearc/conform.nvim',
-    --     opts = {
-    --         formatters_by_ft = { python = { "black" }, cs = { "csharpier" }, },
-    --         formatters = { csharpier = { command = "dotnet", args = { "format", "style", }, }, },
-    --     },
-    -- }
 }
 
 local pics_setup = require('pics')
 pics_setup()
 
-
-local keys = lvim.builtin.which_key.mappings
-
-keys["m"] = {
-    name = "make",
-    r = { "<cmd>!bash -c make run<cr>", "Run `make run`" },
-    b = { "<cmd>!bash -c make build<cr>", "Run `make build`" },
-}
-lvim.builtin.terminal.open_mapping = "<C-/>"
-
+local keybindings_setup = require('keys')
+keybindings_setup()
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
---
+++ keys.lua
@@ -0,0 +1,11 @@
+return function()
+
+local keys = lvim.builtin.which_key.mappings
+
+keys["m"] = {
+    name = "make",
+    r = { "<cmd>!bash -c make run<cr>", "Run `make run`" },
+    b = { "<cmd>!bash -c make build<cr>", "Run `make build`" },
+}
+lvim.builtin.terminal.open_mapping = "<C-/>"
+end
